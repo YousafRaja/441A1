013b54fd44e9b238438ca98bdfa05c24407d5678
Yousaf R <waynestudent38@gmail.com>
2018-02-25 00:54:15 -0700

    Basic file transfer working, still need timers, ACK, and multiprocessing

--- A2/udp_client.cpp
+++ A2/udp_client.cpp
@@ -3,9 +3,123 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>  // Required for stringstreams
+#include <string>
+#include "OctoPro.h"
+using namespace std;
+int BUFFER_SIZE = 8888; //max size of message buffer
+int OCTOBLOCK_SIZE = 8888;
+int OCTOLEG_SIZE = 1111;
+int OCTOMINI_SIZE = 8;
+char PADBYTE = '~';
+
+string sendMsg(string msg, int sock, struct sockaddr_in server_address ){
+
+	// send data
+	const char* data_to_send = msg.c_str();
+	int len = sendto(sock, data_to_send, strlen(data_to_send), 0,
+			(struct sockaddr*) &server_address, sizeof(server_address));
+
+	printf("message has been sent to server\n");
+
+	// received echoed data back
+	char buffer[BUFFER_SIZE];
+	int recv_bytes = recvfrom(sock, buffer, BUFFER_SIZE, 0, NULL, NULL);
+	printf("received bytes = %d\n", recv_bytes);
+	//buffer[len] = '\0';
+	printf("recieved: '%s'\n", buffer);
+	return buffer;
+
+}
+
+
+
+
+string intToString(int i){
+	stringstream ss;
+	ss << i;
+	return ss.str();
+}
+
+int stringToInt(string s){
+	stringstream ss(s);
+	int i;
+	ss >> i;
+	return i;
+}
+
+string requestOctoBlock(string fileName, int sock, struct sockaddr_in server_address, int blockNumber){
+		string data = "OctoGetBlock:" + fileName + " " + intToString(blockNumber);
+		return sendMsg(data, sock, server_address);
+}
+
+string requestOctoLeg(string fileName, int sock, struct sockaddr_in server_address, int blockNumber, int legNumber){
+		string data = "OctoGetLeg:" + fileName + " " +intToString(blockNumber)+" "+intToString(legNumber);
+		return sendMsg(data, sock, server_address);
+}
+
+string requestOctoMinis(string fileName, int sock, struct sockaddr_in server_address, int blockNumber, int legNumber, int miniNumber){
+		string data = "OctoGetMinis:" + fileName + " " +intToString(blockNumber)+" "+intToString(legNumber)+" "+intToString(miniNumber);
+		return sendMsg(data, sock, server_address);
+}
+
+int getFileLength(string fileName, int sock,struct sockaddr_in server_address) {
+	// send data
+	string data = OctoCheck + fileName;
+	string response = sendMsg(data, sock, server_address);
+	return stringToInt(response);
+}
+
+int getPaddedSize(int partialLeg){
+	while(partialLeg%8!=0){
+		partialLeg++;
+	}
+	return partialLeg;
+}
+
+string trimPadding(string data){
+	int i = 0;
+	int s = data.size()-1;
+	for (;i<s; i++){
+		if (data[s-i]==PADBYTE){
+			i++;
+		} else {
+			i--;
+			break;
+		}
+	}
+	return data.substr(0, data.size()-i);
+}
+//string generateRequest(string fileName, int fileLength){
+//
+//}
+
+int getOctoblocks(int fileLength){
+	return fileLength/OCTOBLOCK_SIZE;
+}
+
+int getOctolegs(int blockLength) {
+	return blockLength/OCTOLEG_SIZE;
+}
+
+int getOctoMinis(int legLength) {
+	return (getPaddedSize(legLength)/OCTOMINI_SIZE);
+}
+
+void saveToFile(string filename, string data){
+	ofstream myfile;
+	  myfile.open (filename.c_str());
+	  myfile << data;
+	  myfile.close();
+}
+
 #define PORT 8001
 int main() {
-	const char* server_name  = "localhost";//loopback
+
+
+	const char* server_name = "localhost"; //loopback
 	const int server_port = PORT;
 
 	struct sockaddr_in server_address;
@@ -18,7 +132,6 @@ int main() {
 
 	inet_pton(AF_INET, server_name, &server_address.sin_addr);
 
-
 	server_address.sin_port = htons(server_port);
 
 	// open socket
@@ -28,24 +141,66 @@ int main() {
 		return 1;
 	}
 	printf("client socket created\n");
-	// data that will be sent to the server
+
+	// get filename
+	string filename = "739.txt";
+	//cout <<"Hello, please enter the name of the file you'd like to retrieve" << endl;
+	//cin >> filename;
+
+	string fullResponse = "";
+	int remainingSize = getFileLength(filename, sock, server_address);
+	int b = getOctoblocks(remainingSize);
+	remainingSize -= b*OCTOBLOCK_SIZE;
+
+	int l = getOctolegs(remainingSize);
+	remainingSize -= l*OCTOLEG_SIZE;
+
+	int m = getOctoMinis(remainingSize);
+	remainingSize = m*OCTOMINI_SIZE - remainingSize; //any remaining amount will be pad bytes
+
+
+	for (int i = 0; i < b; i++) {
+		//request full block OctoGetBlock block#
+		fullResponse += requestOctoBlock(filename, sock, server_address, i);
+		//wait till recved
+	}
+
+
+	for (int j = 0; j < l; j++) {//new process for each leg
+		//OctoGetLeg block# leg#
+		fullResponse += requestOctoLeg(filename, sock, server_address, b, j);
+		//wait till recevd
+	}
+
+	if (m){
+		fullResponse += trimPadding(requestOctoMinis(filename, sock, server_address, b, l, m));
+	}
+	//calculate actual remaining size
+	//calculate padded size
+	//new process for miniblock
+	//make request for padded size
+	//create new string of actual size and copy response from  server
+	//fullResponse =trimPadding(fullResponse);
+	//cout <<"CLIENT RECIEVED: "<<trimPadding(fullResponse)<<endl;
+	saveToFile(filename, fullResponse);
+
+// data that will be sent ++to the server
 	const char* data_to_send = "Hi Server!!!";
 
-	// send data
-	int len =
-	    sendto(sock, data_to_send, strlen(data_to_send), 0,
-	           (struct sockaddr*)&server_address, sizeof(server_address));
+// send data
+	int mesLen = 100;
+	int len = sendto(sock, data_to_send, strlen(data_to_send), 0,
+			(struct sockaddr*) &server_address, sizeof(server_address));
 	printf("message has been sent to server\n");
-	// received echoed data back
-	char buffer[100];
-	int recv_bytes=recvfrom(sock, buffer, len, 0, NULL, NULL);
-	printf("received bytes = %d\n",recv_bytes);
-	buffer[len] = '\0';
+// received echoed data back
+	char buffer[500];
+	int recv_bytes = recvfrom(sock, buffer, mesLen, 0, NULL, NULL);
+	printf("received bytes = %d\n", recv_bytes);
+//buffer[mesLen] = '\0';
 	printf("recieved: '%s'\n", buffer);
 
-	// close the socket
+// close the socket
 	close(sock);
 	return 0;
 }
-
 
--- A2/udp_server.cpp
+++ A2/udp_server.cpp
@@ -19,9 +19,129 @@
 #include <arpa/inet.h>
 #include <signal.h>
 #include <netdb.h>
+#include "FileManager.h"
 
+#include <iostream>
+#include <sstream>
+
+#include <bits/stdc++.h>
 #define PORT 8001
+using namespace std;
+
+string intToString(int i) {
+	stringstream ss;
+	ss << i;
+	return ss.str();
+}
+
+int stringToInt(string s) {
+	stringstream ss(s);
+	int i;
+	ss >> i;
+	return i;
+}
+
+string generateResponse(string request) {
+
+	string type;
+	string filename = "";
+	string response = "INVALID REQUEST";
+
+	unsigned int i = 0;
+	for (; i < request.size(); i++) {
+		type += request[i];
+		if (request[i] == ':') {
+			break;
+		}
+	}
+
+	i++; //skip passed ":"
+	FileManager fileManager;
+	int blockSize = 8888;
+	int blockNumber;
+	int legSize = 1111;
+	int legNumber;
+	int miniSize = 8;
+	int miniNumber;
+	string bN, lN, mN = "";
+	if (type == "OctoCheck:") {
+		for (; i < request.size(); i++) {
+			filename += request[i];
+		}
+		int size = fileManager.getFileSize(filename);
+		response = intToString(size);
+	} else if (type == "OctoGetBlock:") {
+		for (; request[i] != ' '; i++) {
+			filename += request[i];
+		}
+		i++;
+		for (; i < request.size(); i++) {
+			bN += request[i];
+			blockNumber = stringToInt(bN);
+		}
+
+		response = fileManager.getFileRange(filename,
+						(blockNumber),
+						blockSize);
+
+	} else if (type == "OctoGetLeg:") {
+		for (; request[i] != ' '; i++) {
+			filename += request[i];
+		}
+		i++;
+		for (; request[i] != ' '; i++) {
+			bN += request[i];
+			blockNumber = stringToInt(bN);
+		}
+		for (; i < request.size(); i++) {
+			lN += request[i];
+			legNumber = stringToInt(lN);
+		}
+
+		response = fileManager.getFileRange(filename,
+						((blockNumber*blockSize) + (legNumber*legSize)),
+						legSize);
+
+	} else if (type == "OctoGetMinis:") {
+
+		for (; request[i] != ' '; i++) {
+			filename += request[i];
+		}
+		i++;//skip past ' '
+		for (; request[i] != ' '; i++) {
+			bN += request[i];
+			blockNumber = stringToInt(bN);
+		}
+		i++;
+		for (; request[i] != ' '; i++) {
+			lN += request[i];
+			legNumber = stringToInt(lN);
+
+		}
+		for (; i < request.size(); i++) {
+			mN += request[i];
+			miniNumber = stringToInt(mN);
+		}
+
+		response = fileManager.getFileRange(filename,
+						((blockNumber*blockSize) + (legNumber*legSize)),
+						(miniNumber*miniSize));
+
+	}
+
+	return response;
+
+}
+
 int main(int argc, char *argv[]) {
+
+	//FileManager fileManager;
+	//string fileName = "fake.txt";
+	//int test = fileManager.getFileSize(fileName);
+	//string fileContents = "";
+	//int fileSize = -1;
+	//fileManager.findFile(fileName, fileContents, fileSize);
+
 	// port to start the server on
 	int SERVER_PORT = PORT;
 
@@ -46,8 +166,8 @@ int main(int argc, char *argv[]) {
 	printf("server socket created\n");
 	// bind it to listen to the incoming connections on the created server
 	// address, will return -1 on error
-	if ((bind(sock, (struct sockaddr *)&server_address,
-	          sizeof(server_address))) < 0) {
+	if ((bind(sock, (struct sockaddr *) &server_address, sizeof(server_address)))
+			< 0) {
 		printf("could not bind socket\n");
 		return 1;
 	}
@@ -55,26 +175,32 @@ int main(int argc, char *argv[]) {
 	// socket address used to store client address
 	struct sockaddr_in client_address;
 	unsigned int client_address_len = sizeof(client_address);
-        char client_name[100];
+	char client_name[100];
 	// run indefinitely
 	while (true) {
 		char buffer[500];
 
 		// read content into buffer from an incoming client
 		int len = recvfrom(sock, buffer, sizeof(buffer), 0,
-		                   (struct sockaddr *)&client_address,
-		                   &client_address_len);
-		
+				(struct sockaddr *) &client_address, &client_address_len);
+
 		// inet_ntoa prints user friendly representation of the
 		// ip address
 		buffer[len] = '\0';
 		printf("received: '%s' from client %s on port %d\n", buffer,
-		       inet_ntoa(client_address.sin_addr),ntohs(client_address.sin_port));
-		
+				inet_ntoa(client_address.sin_addr),
+				ntohs(client_address.sin_port));
+
 		// send same content back to the client ("echo")
-		int sent_len = sendto(sock, buffer, len, 0, (struct sockaddr *)&client_address,
-		      client_address_len);
-		printf("server sent back message:%d\n",sent_len);
+		string response = generateResponse(buffer);
+		cout << "generated response: " << response << endl;
+		int n = response.length();
+		char char_array[n + 1];
+		strcpy(char_array, response.c_str());
+		int sent_len = sendto(sock, char_array, n + 1, 0,
+				(struct sockaddr *) &client_address, client_address_len);
+		printf("server sent back message of size:%d\n", sent_len);
+		printf("server sent back message containing:%s\n", response);
 
 	}
 	close(sock);
